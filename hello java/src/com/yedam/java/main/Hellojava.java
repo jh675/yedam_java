74d3e035fe08e10fcab802e1f71fe42e7a393fde
jh675 <77498665+jh675@users.noreply.github.com>
2021-12-28 09:17:59 +0900

    Update Hellojava.java

--- hello java/src/com/yedam/java/main/Hellojava.java
+++ hello java/src/com/yedam/java/main/Hellojava.java
@@ -4,5 +4,6 @@ public class Hellojava {
 	public static void main(String[] agrs) {
 		System.out.println("Welcome to Java");
 		System.out.println("2021-12-24");
+		System.out.println("2021-12-28");
 	}
 }
